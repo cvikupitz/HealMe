b145bd571f77a07b21effa34e76c6162c4a7402f
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2018-06-08 12:40:08 -0400

    Zachary Chandler added sortable columns to distance and cost

--- Angular Front End/src/app/results/results.component.ts
+++ Angular Front End/src/app/results/results.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { Hospital, InjuryTreatment } from '../hospital';
 
 @Component({
@@ -26,9 +26,47 @@ export class ResultsComponent implements OnInit {
       9.8, [new InjuryTreatment('Broken Bone', 313)])
   ];
 
-  constructor() { }
+  constructor(private el: ElementRef) {
+  }
 
   ngOnInit() {
+    const c = this;
+
+    window.onload = function () {
+
+      const cols = [];
+
+      for (let i = 0; i < 4 + c.injuries.length; i++) {
+        cols[i] = { 'orderable': (i >= 3) };
+      }
+
+      console.log(cols);
+
+      $('#results-table').DataTable({
+        paging: false,
+        info: false,
+        order: [[3, 'asc']],
+        columns: cols
+      });
+
+
+      const searchBar = $('#results-table_filter');
+      const table = $('#results-table');
+      const start = $('#resultsTableStart');
+
+      start[0].appendChild(searchBar[0]);
+      start[0].appendChild(table[0]);
+
+      searchBar[0].style.width = '';
+      table[0].style.width = '';
+
+
+    };
   }
+
+  check(id: string, cls: string): boolean {
+    return $(`#${id}`).hasClass(cls);
+  }
+
 
 }
