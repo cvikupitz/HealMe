4f3bd2d726d89c2215f10d21a4840e642014fb71
cvikupitz <cvikupit@uoregon.edu>
2018-06-12 19:40:01 -0400

    Updated DDL script

--- scripts/P2-DDL.sql
+++ scripts/P2-DDL.sql
@@ -17,7 +17,7 @@ DROP USER healme_admin CASCADE;
  * Create the user/database & credentials
  */
 CREATE USER healme_admin
-IDENTIFIED BY p4ssw0rd123
+IDENTIFIED BY p4ssw0rd123   -- Change as needed
 DEFAULT TABLESPACE users
 TEMPORARY TABLESPACE temp
 QUOTA 10M ON users;
@@ -38,36 +38,60 @@ conn healme_admin/p4ssw0rd123
 /*
  * Defines and creates the tables
  */
-CREATE TABLE Hospitals (
-    HospitalId      NUMBER PRIMARY KEY,
-    Name            VARCHAR2(65),
-    Address         VARCHAR2(100) NOT NULL
+CREATE TABLE Users (
+    UserId          NUMBER PRIMARY KEY,
+    FirstName       VARCHAR(32),
+    LastName        VARCHAR(32),
+    Street1         VARCHAR(64),
+    Street2         VARCHAR(64),
+    City            VARCHAR(50),
+    State           VARCHAR(5),
+    ZipCode         VARCHAR(10),
+    --Copay           NUMBER(10, 2),
+    --Deductible      NUMBER(10, 2),
+    Email           VARCHAR(64),
+    Password        VARCHAR(64)
 );
 
 CREATE TABLE Injuries (
     InjuryId        NUMBER PRIMARY KEY,
-    Name            VARCHAR2(50)
-);
-
-CREATE TABLE Treatments (
-    HospitalId_FK   NUMBER,
-    InjuryId_FK     NUMBER,
-    Cost            NUMBER(8, 2),
-    FOREIGN KEY (HospitalId_FK) REFERENCES Hospitals(HospitalId),
-    FOREIGN KEY (InjuryId_FK) REFERENCES Injuries(InjuryId)
+    Name            VARCHAR2(50),
+    AvgCost         Number(10, 2)
 );
 
 
 /*
  * Create & initialize any/all sequence(s)
  */
--- FIXME
+CREATE SEQUENCE USER_SEQ
+START WITH 1
+INCREMENT BY 1;
+/
+
+CREATE SEQUENCE INJURY_SEQ
+START WITH 1
+INCREMENT BY 1;
+/
  
 
 /*
  * Create all triggers the database will use
  */
--- FIXME
+CREATE OR REPLACE TRIGGER USER_SEQ_TRIGGER
+BEFORE INSERT ON Users
+FOR EACH ROW
+BEGIN
+  SELECT USER_SEQ.NEXTVAL INTO :new.UserId FROM DUAL;
+END;
+/
+
+CREATE OR REPLACE TRIGGER INJURY_SEQ_TRIGGER
+BEFORE INSERT ON Injuries
+FOR EACH ROW
+BEGIN
+  SELECT INJURY_SEQ.NEXTVAL INTO :new.InjuryId FROM DUAL;
+END;
+/
  
  
 /*
